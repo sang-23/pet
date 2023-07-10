994f816f5276fd18bcd07da8d73e4fbdf0f85c9a
NishthaBordia <bordianishtha@gmail.com>
2023-07-10 12:34:03 +0530

    vendor dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use app\Http\Controllers\vendor_dashboard_controller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('vendor_dashboard');
 });
